64adf29a8cdbed97f4daa3c9976f83556a1552a5
liukelin <314566990@qq.com>
2015-12-27 23:29:28 +0800

    class

---
+++ demo/class1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class my11:
+	def my22(self):
+		return "my"
---
+++ demo/class2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import class1
+
+s = class1.my11()
+print s.my22()
