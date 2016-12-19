02f66822a645f142b9cd7d2d8da6e68f90482aac
liu kelin <314566990@qq.com>
2016-12-19 16:25:21 +0800

    a

---
+++ demo/set_data_mysql.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016
+# @author: liukelin
+#
+'''
+test
+'''
+
+import os
+import sys
+import time
+# import yaml
+import torndb
+import db
+import redis
+import random
+# import utils
+import urllib3
+import json 
+from datetime import date, datetime, timedelta
+import common
+
+config = {
+	'mysql':{
+		'host': '127.0.0.1',
+	    'database': 'mail', 
+	    'user': 'db',
+	    'password': '123', 
+	}
+}
+path_ = ''
+
+# 数据库连接实例
+db.mysql = torndb.Connection(**config['mysql'])
+
+file_check = ["txt"]#文件限制
+
+# 读取所有文件
+def get_txt_list(path, thread):
+    try:
+        #print "开始：%s" %(path)
+        for i in os.listdir(path):
+            temp_dir = os.path.join(path,i)
+            if os.path.isdir(temp_dir):
+                get_txt_list(temp_dir,thread);
+            else:
+                if i.split('.')[-1].lower() in file_check:
+                    # dirs[thread].append(temp_dir)
+                    # shutil.copyfile(temp_dir,os.path.join(file_src,thread,i))
+                    
+                    # 读取文件
+                    get_txt(i)
+                                        
+                    print "线程:%s ,copy:%s" %(thread,temp_dir)
+    except:
+        print 'Have no legal power'
+    return True
+
+def get_txt(dir_):
+	num = 5000 # 5000入库一次
+	i = 0
+
+	f = open(dir_, "r")  
+	while True:  
+	    line = f.readline()  
+	    if line:
+	        line=line.strip()
+	        if line:
+	        	d = line.split('----')
+	        	if i>0 and i%num==0:
+	        		set_data(d)
+	        	i++
+	    else:  
+	        break
+	f.close()
+
+
+
+# 批量入库
+def set_data(body=[]):
+	d = []
+	for i in body:
+		if i[0] and i[1]:
+			msg = "(%s,'%s')" %(i[0],i[1])
+			d.append(msg)
+	sql = " install iinto netease_mail (`mail`,`password`) values %s " % ','.join(d)
+	return db.mysql.execute(sql)
+
+
+if __name__=='__main__':
+	get_txt_list(path_, 1)
+
+
+
+
+
+
