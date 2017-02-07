e856538c462148a00f47778bf26b98bdfdeb16ca
liu kelin <314566990@qq.com>
2017-02-07 16:55:57 +0800

    获取网页截图

--- selenium/web_capture.py
+++ selenium/web_capture.py
@@ -2,10 +2,28 @@
 # -*- coding: utf-8 -*-
 # 网页截屏
 # 
-# # @author: liukelin  314566990@qq.com
+# # @author: liukelin  
 import os
 import sys
 from selenium import webdriver
+import time
+ 
+def web_capture(url, save="capture.png"):
+    # browser = webdriver.Firefox()
+    # 使用内核打开浏览器
+    browser = webdriver.Chrome(executable_path = 'driver/chrome/chromedriver-mac32')
 
-def web_capture():
-    
+
+    browser.set_window_size(1200, 900)
+    browser.get(url) # Load page
+
+    browser.implicitly_wait(2)
+
+    time.sleep(1)
+
+    browser.save_screenshot(save)
+    browser.close()
+ 
+ 
+if __name__ == "__main__":
+    web_capture("http://www.redis.net.cn/tutorial/3511.html", 'capture1.png')
