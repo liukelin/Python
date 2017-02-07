680a53603052b55cb92cf9edeb9ae31fcc3fd383
liu kelin <314566990@qq.com>
2017-02-07 15:07:06 +0800

    数据去重

---
+++ netease_mail/group_mail.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016
+# @author: liukelin
+#
+'''
+    去除mysql数据表中重复数据
+    表数据太大，运行sql语句卡死线程。
+    使用redis set集合排重 并入库
+'''
+import os
+import time
+import torndb
+import redis
+
+config = {
+    'mysql':{
+        'host': '127.0.0.1',
+        'database': 'netease_mail', 
+        'user': 'root',
+        'password': '123456', 
+    },
+    'redis':{
+        'host': '127.0.0.1',
+        'port': 6379,
+        'db':0 
+    }
+}
+
+# 数据库连接实例
+myConn = torndb.Connection(**config['mysql'])
+
+pool = redis.ConnectionPool(**config['redis'])
+redisConn = redis.Redis(connection_pool=pool)
+
+def group_mail():
+    page = 0
+    limit = 5000000
+    key = 'zset_group_mail'
+    while True:
+        page += 1
+        page = (page-1) * limit
+        olist = myConn.query(" select * from `duobao_user_join` limit %s,%s ", page, limit)
+        if not olist:
+            break
+        # zset = []
+        for i in olist:
+            val = "%s====%s" %(i['uid'], i['msg'])
+            redisConn.sadd(key, val)
+        print "page:%s. ok." % str(page)
+
+    return True
+        
+if __name__=='__main__':
+    group_mail()
+
+
+
+
+
+
+
+
+
+
+
+
